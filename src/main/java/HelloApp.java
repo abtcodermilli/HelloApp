0b10aa3c461b4601bcf07c6cb2b431de4cee8a9b
abtcodermilli <thapaankur2000@gmail.com>
2026-04-03 21:53:30 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-       
-        if (args.length > 0) {
-            
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
+        // UC3: Handle optional argument with a default using Ternary Operator
+        // If args.length > 0 is true, use args[0]. If false, use "World".
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
     }
 }
