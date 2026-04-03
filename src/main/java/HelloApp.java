77dca05967dabc4fe58d7629a7adfea65e38d33d
abtcodermilli <thapaankur2000@gmail.com>
2026-04-03 22:20:24 +0530

    Update HelloApp.java

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,7 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // UC3: Handle optional argument with a default using Ternary Operator
-        // If args.length > 0 is true, use args[0]. If false, use "World".
         String name = (args.length > 0) ? args[0] : "World";
 
         System.out.println("Hello, " + name + "!");
